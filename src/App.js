5822c93c1c7482afdeb9119f338288e71643b2a5
sridharyenuka <sridhar.yenuka@gmail.com>
2024-09-10 17:02:27 +0530

    added default login

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NoteState from "./context/notes/NoteState";
 import Alert from "./components/Alert";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import Clickhome from "./components/Clickhome";
 import { useState } from "react";
 function App() {
   const [alert, setAlert] = useState(null);
@@ -28,10 +29,11 @@ function App() {
         <Alert alert={alert}/>
         <div className="container">
         <Routes>
-          <Route exact path="/" element={<Home showAlert={showAlert}/>}/>
+          <Route exact path="/"  element={<Home showAlert={showAlert}/>}/>
           <Route exact path="/about" element={<About />}/>
           <Route exact path="/login" element={<Login showAlert={showAlert}/>}/>
           <Route exact path="/signup" element={<Signup showAlert={showAlert}/>}/>
+          <Route path="*" element={<Clickhome/>}/>
         </Routes>
         </div>
       </Router>
